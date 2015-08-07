e30bd376b76957968f123194736718f18198b09e
Simon Asika <asika32764@gmail.com>
2015-08-07 20:10:37 +0800

    query test refactor

---
+++ Test/AbstractQueryTestCase.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * Part of Windwalker project.
+ *
+ * @copyright  Copyright (C) 2015 {ORGANIZATION}. All rights reserved.
+ * @license    GNU General Public License version 2 or later;
+ */
+
+namespace Windwalker\Database\Test;
+
+use Windwalker\Test\Helper\TestStringHelper;
+use Windwalker\Test\TestCase\AbstractBaseTestCase;
+
+/**
+ * The AbstractQueryBuilderTestCase class.
+ *
+ * @since  {DEPLOY_VERSION}
+ */
+class AbstractQueryTestCase extends AbstractBaseTestCase
+{
+	/**
+	 * Property quote.
+	 *
+	 * @var  array
+	 */
+	protected static $quote = array('"', '"');
+
+	/**
+	 * quote
+	 *
+	 * @param string $text
+	 *
+	 * @return  string
+	 */
+	protected function qn($text)
+	{
+		return TestStringHelper::quote($text, static::$quote);
+	}
+
+	/**
+	 * format
+	 *
+	 * @param   string  $sql
+	 *
+	 * @return  String
+	 */
+	protected function format($sql)
+	{
+		return \SqlFormatter::format((string) $sql, false);
+	}
+}
